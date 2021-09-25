b1c52be368e101f11e2b6dc91a1d26aa34883767
NikitaKirin <62830799+NikitaKirin@users.noreply.github.com>
2021-09-26 00:12:34 +0500

    Написана логика выхода пользователя.

---
+++ app/Http/Controllers/Api/Auth/LogoutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LogoutController extends Controller
+{
+    public function __invoke( Request $request ) {
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => 'You are successfully logged out',
+        ]);
+    }
+}
