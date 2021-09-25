d4eb57dc6a4727858a19dcb8ca0a6be58750e06e
NikitaKirin <62830799+NikitaKirin@users.noreply.github.com>
2021-09-26 00:16:51 +0500

    Добавлены api - маршруты для регистрации и авторизации

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function ( Request $request ) {
     return $request->user();
+});
+
+Route::namespace('App\\Http\\Controllers\\Api')->group(function () {
+    Route::namespace('Auth')->group(function () {
+        Route::post('login', 'LoginController');
+        Route::post('register', 'RegisterController');
+        Route::post('logout', 'LogoutController');
+    });
 });
