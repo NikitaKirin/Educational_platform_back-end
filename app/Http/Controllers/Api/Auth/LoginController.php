c7415af529294bb4d08cc08b2ffab1d3d152b48f
NikitaKirin <62830799+NikitaKirin@users.noreply.github.com>
2021-09-26 00:11:13 +0500

    Написана логика авторизации.

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Auth\LoginRequest;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    const LOGIN_REQUEST_RULES = [
+        'email'    => 'required|email',
+        'password' => 'string|required',
+    ];
+
+    const LOGIN_REQUEST_MESSAGES = [
+        'required' => 'Данное поле обязательно для заполнения',
+        'email'    => 'Вы ввели некорректный email',
+    ];
+
+    public function __invoke( Request $request ) {
+
+        $validator = Validator::make($request->all(), self::LOGIN_REQUEST_RULES, self::LOGIN_REQUEST_MESSAGES);
+
+        if ( $validator->fails() ) {
+            $errors = $validator->errors();
+            return response()->json(["messages" => $errors], 422);
+        }
+
+        if ( !Auth::attempt($request->all()) ) {
+            return response()->json([
+                'message' => 'You cannot sign with those credentials',
+                'errors'  => 'Unauthorised',
+            ], 401);
+        }
+
+        $token = Auth::user()->createToken(config('app.name'));
+
+        $token->token->save();
+
+        return response()->json([
+            'token_type' => 'Bearer',
+            'token'      => $token->accessToken,
+            'expires_at' => Carbon::parse($token->token->expires_at)->toDateTimeString(),
+        ], 200);
+    }
+}
---
+++ app/Http/Requests/Api/Auth/LoginRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Api\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    public function rules() {
+        return [
+            'email'    => 'required|email',
+            'password' => 'string|required',
+        ];
+    }
+
+    public function messages() {
+        return [
+            'required' => 'Данное поле обязательно для заполнения',
+            'email'    => 'Вы ввели некорректный email',
+        ];
+    }
+
+    public function withValidator( $validator ) {
+        if ( $validator->fails() ) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ]);
+        }
+    }
+
+    public function authorize() {
+        return true;
+    }
+}
