a9eb087474b9bf748f81d966a16438c94e2c33cf
NikitaKirin <62830799+NikitaKirin@users.noreply.github.com>
2021-09-28 20:06:30 +0500

    ForceJsonResponse внесён в провайдер маршрутизации

--- routes/api.php
+++ routes/api.php
@@ -14,18 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('json.response')->group(function () {
+Route::middleware('auth:api')->get('/user', function ( Request $request ) {
+    return $request->user();
+});
 
-    Route::middleware('auth:sanctum')->get('/user', function ( Request $request ) {
-        return $request->user();
-    });
-
-    Route::namespace('App\\Http\\Controllers\\Api')->group(function () {
-        Route::namespace('Auth')->group(function () {
-            Route::post('login', 'LoginController');
-            Route::post('register', 'RegisterController');
-            Route::post('logout', 'LogoutController')->middleware('auth:api');
-        });
+Route::namespace('App\\Http\\Controllers\\Api')->group(function () {
+    Route::namespace('Auth')->group(function () {
+        Route::post('login', 'LoginController');
+        Route::post('register', 'RegisterController');
+        Route::post('logout', 'LogoutController')->middleware('auth:api');
     });
 });
 
