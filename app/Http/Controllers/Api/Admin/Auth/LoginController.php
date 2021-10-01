129f0853955d1a8fe7589be5db3f12650e24acc3
NikitaKirin <62830799+NikitaKirin@users.noreply.github.com>
2021-10-01 22:47:17 +0500

    Удаление лишнего кода

--- app/Http/Controllers/Api/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Api/Admin/Auth/LoginController.php
@@ -11,22 +11,7 @@ use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
-/*    const LOGIN_REQUEST_RULES = [
-        'email'    => 'required|email',
-        'password' => 'string|required',
-    ];
-
-    const LOGIN_REQUEST_MESSAGES = [
-        'required' => 'Данное поле обязательно для заполнения',
-        'email'    => 'Вы ввели некорректный email',
-    ];*/
-
     public function __invoke( LoginRequest $request ) {
-       /* $validator = Validator::make($request->all(), self::LOGIN_REQUEST_RULES, self::LOGIN_REQUEST_MESSAGES);
-        if ( $validator->fails() ) {
-            $errors = $validator->errors();
-            return response()->json(["messages" => $errors], 422);
-        }*/
 
         if ( Auth::attempt($request->all()) && Auth::user()->role == 'admin' ) {
             $token = Auth::user()->createToken(config('app.name'));
