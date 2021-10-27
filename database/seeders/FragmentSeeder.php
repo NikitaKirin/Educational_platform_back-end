025060af7fab9737974e184f28896c673ce471ad
NikitaKirin <kirinnikita1406@gmail.com>
2021-10-28 00:18:08 +0500

    Исправлен seeder для сущности "Статья". Добавлен seeder для сущности "Тест".

--- database/seeders/FragmentSeeder.php
+++ database/seeders/FragmentSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Article;
+use App\Models\Test;
 use App\Models\Fragment;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -18,6 +19,48 @@ class FragmentSeeder extends Seeder
             $fragment = new Fragment(['title' => Str::random(10)]);
             $fragment->user()->associate($user);
             $fragment->fragmentgable()->associate($article);
+            $fragment->save();
+        }
+        for ( $i = 0; $i <= 200; $i++ ) {
+            $user = User::find(rand(1, 100));
+            $test = new Test([
+                'content' => json_encode([
+                    "title" => "Example-test",
+                    "main"  => [
+                        "Question_1" => [
+                            "question" => "title_question",
+                            "type"     => "one",
+                            "answers"  => [
+                                "answer_1" => "true",
+                                "answer_2" => "false",
+                                "answer_3" => "false",
+                            ],
+                        ],
+                        "Question_2" => [
+                            "question" => "title_question",
+                            "type"     => "many",
+                            "answers"  => [
+                                "answer_1" => "true",
+                                "answer_2" => "true",
+                                "answer_3" => "false",
+                            ],
+                        ],
+                        "Question_3" => [
+                            "question" => "title_question",
+                            "type"     => "word",
+                            "answers"  => [
+                                "answer_1" => "true",
+                                "answer_2" => "false",
+                                "answer_3" => "false",
+                            ],
+                        ],
+                    ],
+                ]),
+            ]);
+            $test->save();
+            $fragment = new Fragment(['title' => Str::random(10)]);
+            $fragment->user()->associate($user);
+            $fragment->fragmentgable()->associate($test);
             $fragment->save();
         }
     }
