8eb2c087c80e6b41d8b1549eead3ac7280f8575c
NikitaKirin <kirinnikita1406@gmail.com>
2021-10-27 23:22:04 +0500

    Доработана валидация при создании нового фрагмента любого типа.

--- app/Http/Requests/Api/Fragment/CreateFragmentRequest.php
+++ app/Http/Requests/Api/Fragment/CreateFragmentRequest.php
@@ -14,14 +14,32 @@ class CreateFragmentRequest extends FormRequest
         ];
     }
 
-    public function messages() {
+    public function messages(): array {
         return [
             'required' => 'Данное поле обязательно для заполнения',
-            'string'   => 'Введены недоступные символы',
+            'string'   => 'Введены недопустимые символы',
         ];
     }
 
     public function authorize(): bool {
         return true;
+    }
+
+    public function withValidator( $validator ) {
+        $validator->after(function ( $validator ) {
+            if ( $this->input('type') == 'article' ) {
+                $this->validate(['content' => 'string'], ['string' => 'Введены недопустимые символы']);
+            }
+            elseif ( $this->input('type') == 'test' ) {
+                $this->validate(['content' => 'json'], ['json' => 'Ожидались данные в формате JSON']);
+            }
+            elseif ( $this->input('type') == 'video' ) {
+                $this->validate(['content' => 'file|mimes:mp4,ogx,oga,ogv,ogg,webm,qt|mimetypes:video/avi,video/mpeg,video/quicktime,video/mp4'], [
+                    'file'      => 'На вход ожидался файл',
+                    'mimes'     => 'Поддерживаются файлы со следующими расширениями: :values',
+                    'mimetypes' => 'Поддерживаются файлы следующего формата :values',
+                ]);
+            }
+        });
     }
 }
