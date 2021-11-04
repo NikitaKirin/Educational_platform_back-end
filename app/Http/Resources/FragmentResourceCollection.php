511db23be5059199960e485139fe2d8bce05896c
NikitaKirin <kirinnikita1406@gmail.com>
2021-11-04 14:40:27 +0500

    Отдельной переменной выведено общее количество записей - фрагментов в БД.

--- app/Http/Resources/FragmentResourceCollection.php
+++ app/Http/Resources/FragmentResourceCollection.php
@@ -4,14 +4,19 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 /** @see \App\Models\Fragment */
 class FragmentResourceCollection extends ResourceCollection
 {
     public static $wrap = 'fragments';
+
     public function toArray( $request ): array {
+        $count = DB::table('fragments')->where('deleted_at', '=', null)
+                   ->count(); // Запоминаем общее количество записей в таблице "fragments";
         return [
-            'fragments' => $this->collection,
+            'all_count' => $count,
+            "fragments" => $this->collection,
         ];
     }
 }
