e005d8aeee0f84164d1aa9454fe033f3203efe48
NikitaKirin <kirinnikita1406@gmail.com>
2021-11-08 11:39:19 +0500

    Создана модель тегов.

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -11,4 +11,9 @@ class Tag extends Model
     protected $fillable = [
         'value',
     ];
+
+    // Устанавливаем связь "многие со многим" с таблицей "fragments" через промежуточную таблицу "fragment_tag"
+    public function fragments(): \Illuminate\Database\Eloquent\Relations\BelongsToMany {
+        return $this->belongsToMany(Fragment::class);
+    }
 }
