13d73be6155fab6f131bbfc339979af83e5786ef
NikitaKirin <kirinnikita1406@gmail.com>
2021-11-11 11:13:21 +0500

    Рефакторинг.

--- app/Http/Resources/TagResourceCollection.php
+++ app/Http/Resources/TagResourceCollection.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 /** @see \App\Models\Tag */
 class TagResourceCollection extends ResourceCollection
 {
+    public static $wrap = 'tags';
     public function toArray( $request ): array {
         return [
             'data' => $this->collection,
