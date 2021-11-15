2f0e6da1da2d77c39b526f0885b75508f760d2b4
NikitaKirin <kirinnikita1406@gmail.com>
2021-11-16 00:07:16 +0500

    Реализована фильтрация фрагментов по тегам.

--- routes/api/user/fragments.php
+++ routes/api/user/fragments.php
@@ -4,17 +4,19 @@ Route::middleware(['auth:api', 'blockUser'])->group(function () {
     Route::get('/teacher/fragments/{user}', 'FragmentController@fragmentsTeacherIndex')
          ->middleware('can:viewTeacherFragments,user')
          ->name('fragments.teacher.index'); // Получить список фрагментов определённого учителя;
-    Route::get('/fragments/like/{title?}{type?}', 'FragmentController@likeIndex')->name('fragments.like.index');
-    Route::post('/fragments', 'FragmentController@store')->middleware('can:create,App\\Models\\Fragment')->name('fragments.store'); // Создать новый фрагмент;
+    Route::get('/fragments/like/{title?}/{type?}/{tags?}', 'FragmentController@likeIndex')
+         ->name('fragments.like.index'); // Получить список избранных фрагментов (текущий пользователь);
+    Route::post('/fragments', 'FragmentController@store')->middleware('can:create,App\\Models\\Fragment')
+         ->name('fragments.store'); // Создать новый фрагмент;
     Route::get('/fragments/{fragment}', 'FragmentController@show')
          ->name('fragments.show'); // Получить данные об определенном фрагменте;
     Route::patch('/fragments/{fragment}', 'FragmentController@update')->middleware('can:update,fragment')
          ->name('fragments.update'); // Обновить данные фрагмента;
     Route::delete('/fragments/{fragment}', 'FragmentController@destroy')->middleware('can:delete,fragment')
          ->name('fragments.destroy'); // Удалить фрагмент;
-    Route::get('/my-fragments/{title?}/{type?}', 'FragmentController@myIndex')
+    Route::get('/my-fragments/{title?}/{type?}/{tags?}', 'FragmentController@myIndex')
          ->name('fragments.index.my'); // Посмотреть список фрагментов текущего пользователя;
-    Route::get('/fragments/{title?}/{type?}', 'FragmentController@index')
+    Route::get('/fragments/{title?}/{type?}/{tags?}', 'FragmentController@index')
          ->name('fragments.index'); // Посмотреть список всех фрагментов;
     Route::put('/fragments/{fragment}', 'FragmentController@like')
          ->name('fragments.like'); // Добавить/удалить фрагмент из избранного;
