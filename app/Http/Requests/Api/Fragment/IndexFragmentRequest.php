2fffb916b99bd70a816b0e4fa8dc03f302ea0ec8
NikitaKirin <kirinnikita1406@gmail.com>
2021-12-19 12:09:02 +0500

    Доработана валидация для поиска фрагментов по типу "изображение".

--- app/Http/Requests/Api/Fragment/IndexFragmentRequest.php
+++ app/Http/Requests/Api/Fragment/IndexFragmentRequest.php
@@ -12,7 +12,7 @@ class IndexFragmentRequest extends FormRequest
         $tags = Tag::getValues();
         return [
             'title' => ['nullable', 'string'],
-            'type'  => ['nullable', 'string', 'in:article,test,video'],
+            'type'  => ['nullable', 'string', 'in:article,test,video,image'],
             'tags'  => ['nullable', 'array', Rule::in($tags)],
         ];
     }
