962be98c6bd555a6cf54c7063aad217990dcfb66
Nikita Kirin <kirinnikita1406@gmail.com>
2022-03-03 14:32:57 +0500

    feat[games]: add migration file for new fragment's type "game"

---
+++ database/migrations/2022_03_03_143001_create_games_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    public function up() {
+        Schema::create('games', function ( Blueprint $table ) {
+            $table->id();
+            $table->string('type');
+            $table->json('content');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('games');
+    }
+}
