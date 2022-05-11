89e9694e0ad43c1f4ccf3fe12294fe2d8faa736c
NikitaKirin <kirinnikita1406@gmail.com>
2022-05-11 22:57:41 +0500

    feat[GameTypeSeeder]: add value for task column

--- database/seeders/GameTypeSeeder.php
+++ database/seeders/GameTypeSeeder.php
@@ -9,24 +9,32 @@ use Illuminate\Database\Seeder;
 class GameTypeSeeder extends Seeder
 {
     public function run() {
-        $gameType = new GameType(['type' => 'pairs', 'title' => 'Парочки', 'description' => 'Какое-то описание']);
+        $gameType = new GameType([
+            'type'        => 'pairs',
+            'title'       => 'Парочки',
+            'description' => 'Развивающая игра, где необходимо запомнить расположение одинаковых карточек. После начала игры карточки переворачиваются, а игроку надо находить пары изображений.',
+            'task'        => 'Найди одинаковые картинки.',
+        ]);
         $gameType->save();
         $gameTypeSecond = new GameType([
             'type'        => 'matchmaking',
             'title'       => 'Ассоциации',
-            'description' => 'Какое-то описание',
+            'description' => 'Развивающая игра, в которой необходимо соединять изображения по заранее заданному признаку.',
+            'task'        => 'Соедини картинки по признаку, как показано в примере.',
         ]);
         $gameTypeSecond->save();
         $gameTypeThird = new GameType([
             'type'        => 'sequences',
             'title'       => 'Последовательности',
-            'description' => 'Какое-то описание',
+            'description' => 'Развивающая игра, в которой необходимо расположить картинки в заранее заданном порядке.',
+            'task'        => 'Расположи картинки в правильном порядке.',
         ]);
         $gameTypeThird->save();
         $gameTypeFourth = new GameType([
             'type'        => 'puzzles',
             'title'       => 'Пазлы',
-            'description' => 'Какое-то описание',
+            'description' => 'Развивающая игра, суть которой состоит в соединении кусочков изображения единую картинку по образцу.',
+            'task'        => 'Собери картинку.',
         ]);
         $gameTypeFourth->save();
     }
