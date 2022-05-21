3f6bf12ec7a89abb37877198fc2dc3e38c117a13
NikitaKirin <kirinnikita1406@gmail.com>
2022-05-21 13:33:16 +0500

    feat[orchid]: fix headers for web request

--- app/Http/Middleware/ForceJsonResponse.php
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -15,7 +15,7 @@ class ForceJsonResponse
      * @return mixed
      */
     public function handle( Request $request, Closure $next ) {
-        $request->headers->set('Accept', 'application/json');
+        //$request->headers->set('Accept', 'application/json');
         return $next($request);
     }
 }
