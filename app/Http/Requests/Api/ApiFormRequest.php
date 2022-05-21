0a321eaad728f6cb58be1687309dbc810664d8ed
NikitaKirin <kirinnikita1406@gmail.com>
2022-05-21 15:47:15 +0500

    feat[json request]: add class for validation

--- app/Http/Requests/Api/ApiFormRequest.php
+++ app/Http/Requests/Api/ApiFormRequest.php
@@ -2,7 +2,24 @@
 
 namespace App\Http\Requests\Api;
 
-class ApiFormRequest
-{
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Foundation\Http\FormRequest;
 
+abstract class ApiFormRequest extends FormRequest
+{
+    protected function failedValidation( Validator $validator ) {
+        // все ошибки валидации
+        $errors = (new ValidationException($validator))->errors();
+
+        throw new HttpResponseException(response()->json([
+            'errors' => $errors,
+        ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+
+    abstract public function authorize();
+
+    abstract public function rules();
 }
